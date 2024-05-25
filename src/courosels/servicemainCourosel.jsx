f2df371949a25e98b65099479f43ff7744c7974d
RamyaMidde <sathyasoftechin@gmail.com>
2024-05-25 10:45:59 +0530

    dfg

--- src/courosels/servicemainCourosel.jsx
+++ src/courosels/servicemainCourosel.jsx
@@ -1,7 +1,40 @@
-const ServiceCorousel=()=>{
-    let img1=''
-    return(<>
-    dddd
-    </>)
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import one from '../asserts/one.jpg';
+import two from '../asserts/two.jpg';
+import three from '../asserts/three.jpg';
+
+const ServiceCarousel = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={one}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={two}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={three}
+                    alt="Third slide"
+                />
+                {/* <Carousel.Caption>
+                    <h3>Third Slide Label</h3>
+                    <p>Third slide description.</p>
+                </Carousel.Caption> */}
+            </Carousel.Item>
+        </Carousel>
+    );
 }
-export default ServiceCorousel;
+
+export default ServiceCarousel;
